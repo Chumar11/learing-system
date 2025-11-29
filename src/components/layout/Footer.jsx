1e797e7db4136cdb97c168d0a1a5d2f202caab74
Chumar <chumarsarwar3@gmail.com>
2025-11-29 17:16:01 +0500

    commit

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
           <Col xs={12} className="text-center">
             {currentYear} Crafted by <IconifyIcon icon="solar:hearts-bold-duotone" className="fs-18 align-middle text-danger" />{' '}
             <Link href="https://github.com/Majidkhan3" className="" target="_blank">
-              Majid Dev
+              Zaighm Khan
             </Link>
           </Col>
         </Row>
